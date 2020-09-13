Fix for wrong lane potential calculation when lane and ego car not pointing in the same direction

ego_vehicle/src/ego_vehicle/artf_pot_funcs.py
@@ -183,7 +183,7 @@ class CarPotential(object):
         # car_z = Parallel(n_jobs=4)(delayed(func)(obstcl_vehicle) for obstcl_vehicle in self.obstcl_vehicles_plgns)
         # temp = self.plgn_list[0]
         # print("What is this?? : {:s}".format(np.asarray(temp)[0]))
-        return np.clip(np.array(car_z).sum(axis=0),-20,20)
+        return np.array(car_z).sum(axis=0)
 
 
 class LanePotential(object):
@@ -192,11 +192,16 @@ class LanePotential(object):
         super(LanePotential, self).__init__()
         self.lane_edges_transformed = []
         self.curr_waypoint = None
+        self.ego_transform = None
 
-    def update_state(self, curr_waypoint):
+    def update_state(self, curr_waypoint, ego_transform):
         self.curr_waypoint = curr_waypoint
+        self.ego_transform = ego_transform
         lane_edges = Utils.getLaneEdges(curr_waypoint)
-        self.lane_edges_transformed = [Utils.transform_location_R(np.array([lane_edge[0], lane_edge[1], 0]), self.curr_waypoint.transform)[:-1]  for lane_edge in lane_edges]
+        self.lane_edges_transformed = [Utils.transform_location_R(np.array([lane_edge[0], lane_edge[1], 0]),
+                                        ego_transform)[:-1]  for lane_edge in lane_edges]
+        # print("Lane Edges: {:s}").format(lane_edges)
+        # print("Lane Edges Transformed: {:s}").format(self.lane_edges_transformed)
 
     def update(self, pos_meshgrid, json_params):
         """
@@ -212,20 +217,35 @@ class LanePotential(object):
 
         # print(self.lane_edges_transformed)
         U = np.zeros(pos_meshgrid[1].shape)  
-
+        # print("Ego yaw: {:f}").format(Utils.deg360(self.ego_transform.rotation.yaw))
+        # print("Waypoint yaw: {:f}").format(Utils.deg360(self.curr_waypoint.transform.rotation.yaw))
+        theta = -(Utils.deg360(-self.ego_transform.rotation.yaw) - Utils.deg360(-self.curr_waypoint.transform.rotation.yaw))*np.pi/180
+        # theta = 0.0
+        # print("The angle between: {:f}").format(theta*180/np.pi)
         #Calculate Road potential 
+        # if theta < (np.pi/2) and theta > -(np.pi/2): 
         U = U + np.multiply(0.5*json_params["Road_scale_factor"], 
-                                np.divide(1.0,(pos_meshgrid[1]-self.lane_edges_transformed[0][1])**2))
-        U = U + np.multiply(0.5*json_params["Road_scale_factor"], 
-                                np.divide(1.0,(pos_meshgrid[1]-self.lane_edges_transformed[-1][1])**2))
+                            np.divide(1.0,np.square((np.sin(theta)*(pos_meshgrid[0] - self.lane_edges_transformed[0][0])+
+                                                    (np.cos(theta)*(pos_meshgrid[1] - self.lane_edges_transformed[0][1]))))))
 
+        U = U + np.multiply(0.5*json_params["Road_scale_factor"], 
+                            np.divide(1.0,np.square((np.sin(theta)*(pos_meshgrid[0] - self.lane_edges_transformed[-1][0])+
+                                                    (np.cos(theta)*(pos_meshgrid[1] - self.lane_edges_transformed[-1][1]))))))
+        mask1 =  ((np.sin(theta)*pos_meshgrid[0])+(np.cos(theta)*pos_meshgrid[1]) > (np.sin(theta)*self.lane_edges_transformed[0][0] + np.cos(theta)*self.lane_edges_transformed[0][1])) 
+        mask2 =  ((np.sin(theta)*pos_meshgrid[0])+(np.cos(theta)*pos_meshgrid[1]) < (np.sin(theta)*self.lane_edges_transformed[-1][0] + np.cos(theta)*self.lane_edges_transformed[-1][1]))
+        # print(mask1.shape)
+        # print(mask2.shape)
+        mask = mask1&mask2
+        U = np.where(mask, U, 20)
+        
         #Calculate lane potential
         for i,lane in enumerate(self.lane_edges_transformed[1:-1]):
             U = U + np.multiply(json_params["Lane_Alane"], 
-                                np.exp(-np.divide((pos_meshgrid[1]-lane[1])**2, 
-                                (2 * (json_params["Lane_widthfactor"]*abs(self.lane_edges_transformed[i+1][1]-self.lane_edges_transformed[i][1]))**2))))
+                                np.exp(-np.divide(((np.sin(theta)*pos_meshgrid[1])+(np.cos(theta)*pos_meshgrid[0])-(np.sin(theta)*lane[0] + np.cos(theta)*lane[1]))**2, 
+                                (2 * (json_params["Lane_widthfactor"]*abs((np.sin(theta)*self.lane_edges_transformed[i+1][0] + np.cos(theta)*self.lane_edges_transformed[i][1])-
+                                                                            (np.sin(theta)*self.lane_edges_transformed[i][0] + np.cos(theta)*self.lane_edges_transformed[i][1])))**2))))
 
-        return np.clip(U, -20, 20)
+        return U
 
 
 

ego_vehicle/src/ego_vehicle/ego_vehicle.py
@@ -71,7 +71,7 @@ class EgoVehicle(CarlaEgoVehicle):
         # Create mesh for potential calculation
         x = np.arange(self.json_params["X_limit"][0],self.json_params["X_limit"][1], self.json_params["grid_res"])
         y = np.arange(self.json_params["Y_limit"][0],self.json_params["Y_limit"][1], self.json_params["grid_res"])
-        self.pos_meshgrid = np.meshgrid(x, y, sparse=True)
+        self.pos_meshgrid = np.meshgrid(x, y, sparse=False)
 
         # Create car potential class
         self.car_potential = CarPotential()
@@ -85,6 +85,7 @@ class EgoVehicle(CarlaEgoVehicle):
 
         self.fig_2d = plt.figure()
         self.ax_2d = self.fig_2d.add_subplot(111)
+        self.ax_2d.set(xlabel="x (Ego Vehicle Frame)", ylabel="y (Ego Vehicle Frame)", title="View from the top")
         self.surf = None
         # # Define X, Y, Z limits for plots
         # xlims = np.array([-50,50])
@@ -103,7 +104,7 @@ class EgoVehicle(CarlaEgoVehicle):
 
         #Update Lane edges
         curr_waypoint = self.world.get_map().get_waypoint(self.player.get_location(), project_to_road=True)
-        self.lane_potential.update_state(curr_waypoint)
+        self.lane_potential.update_state(curr_waypoint, self.player.get_transform())
         # self.lane_edges = Utils.getLaneEdges(curr_waypoint) 
         
          
@@ -121,12 +122,16 @@ class EgoVehicle(CarlaEgoVehicle):
         # #Update Road, Lane potentials   
         z = z + self.lane_potential.update(self.pos_meshgrid, self.json_params)
 
-        z = np.clip(z,-20,20) 
-        # Update plot ...
-        self.plot_all(z)
-
-    # def select_target(self):
+        self.z = np.clip(z,-20,20) 
         
+        self.select_target()
+
+        # Update plot ...
+        self.plot_all(self.z)
+
+    def select_target(self):
+        self.L = self.z < 8
+        self.L = [self.pos_meshgrid[0][self.L], self.pos_meshgrid[1][self.L]]
 
         
 
@@ -135,11 +140,9 @@ class EgoVehicle(CarlaEgoVehicle):
         #Clear previous plots
         if self.surf is not None: self.surf.remove()
         if self.ax_2d is not None: self.ax_2d.clear()
-        
+        self.ax_2d.set(xlabel="x (Ego Vehicle Frame)", ylabel="y (Ego Vehicle Frame)", title="View from the top")
         # Plot lane markings
-        # curr_waypoint = self.world.get_map().get_waypoint(self.player.get_location(), project_to_road=True)
-        # self.lane_edges = Utils.getLaneEdges(curr_waypoint)
-        # self.ax_2d.plot(np.asarray(self.lane_edges)[:,0]-self.ego_car_loc.x, np.asarray(self.lane_edges)[:,1]-self.ego_car_loc.y, marker='*')
+        [self.ax_2d.plot(lane[0], lane[1], marker='+') for lane in self.lane_potential.lane_edges_transformed]
 
         #Plot ego vehicle and its polygon
         self.ax_2d.plot(self.car_potential.ego_car_location[0], self.car_potential.ego_car_location[1], marker='x')
@@ -156,15 +159,17 @@ class EgoVehicle(CarlaEgoVehicle):
                          [shapely.ops.nearest_points(Point(self.car_potential.ego_car_location), obst_car)[0].y, 
                             shapely.ops.nearest_points(Point(self.car_potential.ego_car_location), obst_car)[1].y]) for obst_car in self.car_potential.obstcl_vehicles_plgns]
 
-       
+        #Plot Reachable / safe set
+        self.ax_2d.scatter(self.L[0], self.L[1], marker='.', color='c')
+
         # Plot potential field as a 3d surface plot
         self.surf = self.ax_3d.plot_surface(self.pos_meshgrid[0], self.pos_meshgrid[1], z, cmap=plt.cm.coolwarm, antialiased=True, linewidth=0, rstride=1, cstride=1)
         # Print min max of pot field
         print("Details of Mesh grid values: Shape={:s}, Min z value={:.2f}, Max z value={:.2f}".format(z.shape, np.amin(z), np.amax(z)))
 
         # #Set axes limits of all plots
-        # self.ax_2d.set_xlim(-30,30)
-        # self.ax_2d.set_ylim(-30,30)
+        self.ax_2d.set_xlim(-30,30)
+        self.ax_2d.set_ylim(-30,30)
         # ax_3d.set_xlim(xlims[0],xlims[1])
         # ax_3d.set_ylim(ylims[0],ylims[1])
         # ax_3d.set_zlim(zlims[0],zlims[1])
@@ -172,7 +177,7 @@ class EgoVehicle(CarlaEgoVehicle):
         
         # self.fig_3d.canvas.draw()
         self.fig_2d.canvas.draw()
-        raw_input("Press Enter to continue...")
+        # raw_input("Press Enter to continue...")
 
 
     def get_subvehicles(self):

ego_vehicle/src/ego_vehicle/lane_pot_test.py
@@ -57,14 +57,16 @@ def main():
     #Calculate Road potential 
     U = U + np.multiply(0.5*json_params["Road_scale_factor"], 
                             np.divide(1.0,(yy-lanes[0][1])**2))
+    U = np.where(yy>lanes[0][1], U, 20)
     U = U + np.multiply(0.5*json_params["Road_scale_factor"], 
                             np.divide(1.0,(yy-lanes[-1][1])**2))
+    U = np.where(yy<lanes[-1][1], U, 20)
 
-    #Calculate lane potential
-    for i,lane in enumerate(lanes[1:-1]):
-        U = U + np.multiply(json_params["Lane_Alane"], 
-                            np.exp(-np.divide((yy-lane[1])**2, 
-                            (2 * (json_params["Lane_widthfactor"]*abs(lanes[i+1][1]-lanes[i][1]))**2))))
+    # #Calculate lane potential
+    # for i,lane in enumerate(lanes[1:-1]):
+    #     U = U + np.multiply(json_params["Lane_Alane"], 
+    #                         np.exp(-np.divide((yy-lane[1])**2, 
+    #                         (2 * (json_params["Lane_widthfactor"]*abs(lanes[i+1][1]-lanes[i][1]))**2))))
 
      
     surf = ax_3d.plot_surface(xx,yy, U,

ego_vehicle/src/ego_vehicle/testmodel.py
@@ -16,37 +16,93 @@ from kinematic_bicycle_model import bicycle_model
 plt.ion()
 
 def main():
-	x_0 = np.array([0.0,0.0,0.0,5.0])
-	u_r1 = np.array([1.0, 0.1])
-	u_r2 = np.array([1.0, -0.1])
-	
-	# Get timesteps
-	delta_t = 0.1 # time step size (seconds)
-	t_max = 5 # max sim time (seconds)
-	t = np.linspace(0, t_max, t_max/delta_t)
+    # Read params from file
+    param_definition_file = "/home/jiyo/workspace/autonomous-overtaking/autonomous-overtaking/ego_vehicle/config/params.json"
+    if not os.path.exists(param_definition_file):
+        raise RuntimeError(
+            "Could not read param-definition from {}".format(param_definition_file))
+    json_params = None
+    with open(param_definition_file) as handle:
+        json_params = json.loads(handle.read())
 
-	dx_vals_int_1 = integrate.odeint(bicycle_model, x_0, t, args=(u_r1,))
+    # Add fig and axes for contour and 3d surface plots
+    fig_3d = plt.figure()
+    ax_3d = fig_3d.add_subplot(111, projection='3d')
+    ax_3d.set(xlabel="x", ylabel="y", zlabel="f(x, y)", title="3D Surface plot of Risk Map")
 
-	dx_vals_int_2 = integrate.odeint(bicycle_model, x_0, t, args=(u_r2,))		
+    fig_2d = plt.figure()
+    ax_2d = fig_2d.add_subplot(111)
 
-	plt.plot(dx_vals_int_1[:, 0], dx_vals_int_1[:, 1], 'r', label='xy plot(t)')
-	plt.plot(dx_vals_int_2[:, 0], dx_vals_int_2[:, 1], 'r', label='xy plot(t)')
+    # Define X, Y, Z limits for plots
+    xlims = np.array(np.array([-50,50]))
+    ylims = np.array([-50,50])
+    zlims = np.array([0,30])
 
-	reach_set_tup1 = zip(dx_vals_int_1[:, 0],dx_vals_int_1[:, 1])
-	reach_set_tup2 = zip(dx_vals_int_2[:, 0],dx_vals_int_2[:, 1])
-	reach_set_tup1.reverse()
-	reach_set_tup1.extend(reach_set_tup2)
-	reach_set = LinearRing(reach_set_tup1)
+    # Define X, Y limits for area under consideration
+    x_vision_limit = np.array(json_params["X_limit"])
+    y_vision_limit = np.array(json_params["Y_limit"])
+
+    # Create mesh
+    x = np.arange(x_vision_limit[0],x_vision_limit[1], 0.5)
+    y = np.arange(y_vision_limit[0],y_vision_limit[1], 0.5)
+    pos_meshgrid = np.meshgrid(x, y, sparse=False, copy=True)
+    
+    
+    lanes = [(1,-3.5),(1,3.5),(1,10.5),(1,17.5)]
 
 
-	plt.plot(np.asarray(reach_set)[:,0],np.asarray(reach_set)[:,1])
+    theta = 45*np.pi/180
+    z = np.multiply(0.5*json_params["Road_scale_factor"], 
+                                np.divide(1.0,np.square((np.sin(theta)*pos_meshgrid[1])+(np.cos(theta)*pos_meshgrid[0])-lanes[0][1])))
+    z = np.where((np.sin(theta)*pos_meshgrid[1])+(np.cos(theta)*pos_meshgrid[0])>lanes[0][1], z, 20)
+    # z = np.square(pos_meshgrid[0])
+    # z = 1/np.square((np.sin(theta)*pos_meshgrid[0])+(np.cos(theta)*pos_meshgrid[1]))
+
+    z = np.clip(z,-20,20)
+    # Plot potential field as a 3d surface plot
+    surf = ax_3d.plot_surface(pos_meshgrid[0],
+                                pos_meshgrid[1], z, 
+                                cmap=plt.cm.coolwarm, 
+                                antialiased=True, 
+                                linewidth=0, 
+                                rstride=1, cstride=1)
+    print(z)
+    print(z.shape)
+
+    l = np.where(z>4)
+
+    print(l)
+    # print(z[l])
+
+    # x_0 = np.array([0.0,0.0,0.0,5.0])
+    # u_r1 = np.array([1.0, 0.1])
+    # u_r2 = np.array([1.0, -0.1])
+    
+    # # Get timesteps
+    # delta_t = 0.1 # time step size (seconds)
+    # t_max = 5 # max sim time (seconds)
+    # t = np.linspace(0, t_max, t_max/delta_t)
+
+    # dx_vals_int_1 = integrate.odeint(bicycle_model, x_0, t, args=(u_r1,))
+
+    # dx_vals_int_2 = integrate.odeint(bicycle_model, x_0, t, args=(u_r2,))     
+
+    # reach_set_tup1 = zip(dx_vals_int_1[:, 0],dx_vals_int_1[:, 1])
+    # reach_set_tup2 = zip(dx_vals_int_2[:, 0],dx_vals_int_2[:, 1])
+    # reach_set_tup1.reverse()
+    # reach_set_tup1.extend(reach_set_tup2)
+    # reach_set = LinearRing(reach_set_tup1)
+
+    # reach_safe_set = 
+
+    # plt.plot(np.asarray(reach_set)[:,0],np.asarray(reach_set)[:,1])
 
 
-	plt.legend(loc='best')
-	plt.xlabel('t')
-	plt.grid()
-	plt.show()
-	raw_input("Press Enter to continue...")
+    # plt.legend(loc='best')
+    # plt.xlabel('t')
+    # plt.grid()
+    # plt.show()
+    raw_input("Press Enter to continue...")
 
 
 